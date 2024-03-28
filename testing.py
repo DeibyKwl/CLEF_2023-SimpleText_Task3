2287709a76f332b0bd7a6d1e8c36d5f340f20742
DeibyKwl <deibykwl@gmail.com>
2024-03-28 04:04:20 -0400

    testing script

---
+++ testing.py
@@ -0,0 +1,10 @@
+# Use a pipeline as a high-level helper
+from transformers import pipeline
+
+api_token = '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'
+
+pipe = pipeline("text-generation", model="meta-llama/Llama-2-7b-chat-hf", token=api_token)
+
+# Generate text using the loaded model
+generated_text = pipe("How are you?")
+print(generated_text)
