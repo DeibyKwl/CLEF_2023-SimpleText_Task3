86c8e6a802e2cd3dbc54806d1fb0812abd7c7d13
DeibyKwl <deibykwl@gmail.com>
2024-04-29 20:39:52 -0400

    Removed metrics calculation

--- Task3_Llama3.py
+++ Task3_Llama3.py
@@ -3,10 +3,6 @@ import torch
 import sys
 import transformers
 
-import textstat
-from evaluate import load
-from nltk.translate.bleu_score import sentence_bleu
-
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 # if torch.cuda.is_available():
@@ -60,69 +56,16 @@ def create_tsv(snt_ids, summaries):
         "simplified_snt": summaries
     })
 
-    results_df.to_csv("Task3_TopGap_Llama3_2.tsv", sep='\t', index=False)
-
-def get_simplified_tsv_data():
-    data = pd.read_csv('Task3_TopGap_Llama3_2.tsv', sep='\t')
-    return data['simplified_snt'].tolist()
-
-def get_qrels_tsv_data(file_path):
-    data = pd.read_csv(file_path, sep='\t')
-    return data['simplified_snt'].tolist()
-
-def create_tsv_metrics(snt_ids, source_sentences, simplified_snts, qrels_snts):
-
-    fkgl_source = []
-    fkgl_simplify = []
-    sari_scores = []
-    bleu_scores = []
-    compression_ratios = []
-
-    for source_sentence, simplify_sentence, qrels_sentence in zip(source_sentences, simplified_snts, qrels_snts):
-        fkgl_sentence1 = round(textstat.flesch_kincaid_grade(source_sentence), 3)
-        fkgl_sentence2 = round(textstat.flesch_kincaid_grade(simplify_sentence), 3)
-        fkgl_source.append(fkgl_sentence1)
-        fkgl_simplify.append(fkgl_sentence2)
-
-        sari = load("sari")
-        sari_score = sari.compute(sources=[source_sentence], predictions=[simplify_sentence], references=[[qrels_sentence]])
-        sari_scores.append(round(sari_score['sari'], 3))
-
-        reference = [ref[0].split() for ref in [[qrels_sentence]]]
-        candidate = simplify_sentence.split()
-        bleu_score = round(sentence_bleu(reference, candidate), 3)
-        bleu_scores.append(bleu_score)
-
-        compression_ratio = round(len(simplify_sentence) / len(source_sentence), 3)
-        compression_ratios.append(compression_ratio)
-
-    results_df = pd.DataFrame({
-        "snt_id": snt_ids,
-        "fkgl_source": fkgl_source,
-        "fkgl_simplify": fkgl_simplify,
-        "sari": sari_scores,
-        "bleu": bleu_scores,
-        "Compression Ratio": compression_ratios
-    }) 
-
-    results_df.to_csv("Task3_metrics_comparison_Llama3_2.tsv", sep='\t', index=False)
-
+    results_df.to_csv("Task3_TopGap_Llama3.tsv", sep='\t', index=False)
 
 def main():
-    if len(sys.argv) != 3:
-        print('Usage: python Task3_Llama3.py <filename1> <filename2>')
+    if len(sys.argv) != 2:
+        print('Usage: python Task3_Llama3.py <filename1>')
         return
     file_path = sys.argv[1]
     snt_ids, source_sentences, query_texts = get_tsv_data(file_path)
-
     simplified_texts = simplify_text(source_sentences)
-
     create_tsv(snt_ids, simplified_texts)
-    simplified_snts = get_simplified_tsv_data()
-
-    file_path2 = sys.argv[2]
-    qrels_snts = get_qrels_tsv_data(file_path2)
-    create_tsv_metrics(snt_ids, source_sentences, simplified_snts, qrels_snts)
 
 if __name__ == '__main__':
     main()
